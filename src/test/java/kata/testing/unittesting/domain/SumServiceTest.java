5be957c9a8db632a347f4a927a0c416ff6ce5aa4
Ben WU <wubin28@gmail.com>
2018-12-19 14:49:13 +0800

    add a test for zero score

--- src/test/java/kata/testing/unittesting/domain/SumServiceTest.java
+++ src/test/java/kata/testing/unittesting/domain/SumServiceTest.java
@@ -7,7 +7,17 @@ import org.mockito.Mockito;
 public class SumServiceTest {
 
     @Test
-    public void should_sum_using_mock_of_score_repository() {
+    public void should_sum_zero_score_using_mock_of_score_repository() {
+        SumService sumService = new SumService();
+        ScoreRepository scoreRepositoryMock = Mockito.mock(ScoreRepository.class);
+        Mockito.when(scoreRepositoryMock.retrieveAllScores()).thenReturn(new int[] {});
+        sumService.setSomeDataService(scoreRepositoryMock);
+
+        Assert.assertEquals(0, sumService.sum());
+    }
+
+    @Test
+    public void should_sum_three_scores_using_mock_of_score_repository() {
         SumService sumService = new SumService();
         ScoreRepository scoreRepositoryMock = Mockito.mock(ScoreRepository.class);
         Mockito.when(scoreRepositoryMock.retrieveAllScores()).thenReturn(new int[] {1, 2, 3});
