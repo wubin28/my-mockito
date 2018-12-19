94c3ab0297c5c29cb882d0d25512c28e7ef4142e
Ben WU <wubin28@gmail.com>
2018-12-19 17:36:52 +0800

    the mock should return twice

--- src/test/java/kata/testing/unittesting/domain/ListTest.java
+++ src/test/java/kata/testing/unittesting/domain/ListTest.java
@@ -11,10 +11,19 @@ import static org.mockito.Mockito.when;
 public class ListTest {
 
     @Test
-    public void should_mock_once() {
+    public void should_mock_returning_once() {
         List<Object> listMock = mock(List.class);
         when(listMock.size()).thenReturn(5);
-        
+
         assertEquals(5, listMock.size());
+    }
+
+    @Test
+    public void should_mock_returning_twice() {
+        List<Object> listMock = mock(List.class);
+        when(listMock.size()).thenReturn(5).thenReturn(10);
+
+        assertEquals(5, listMock.size());
+        assertEquals(10, listMock.size());
     }
 }
