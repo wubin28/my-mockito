de858637b0ee727394249419d166985c4130e68c
Ben WU <wubin28@gmail.com>
2018-12-19 14:52:07 +0800

    using properties and @Before in the tests

--- src/test/java/kata/testing/unittesting/domain/SumServiceTest.java
+++ src/test/java/kata/testing/unittesting/domain/SumServiceTest.java
@@ -1,27 +1,30 @@
 package kata.testing.unittesting.domain;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class SumServiceTest {
 
+    SumService sumService = new SumService();
+    ScoreRepository scoreRepositoryMock = Mockito.mock(ScoreRepository.class);
+
+    @Before
+    public void before() {
+        sumService.setSomeDataService(scoreRepositoryMock);
+    }
+
     @Test
     public void should_sum_zero_score_using_mock_of_score_repository() {
-        SumService sumService = new SumService();
-        ScoreRepository scoreRepositoryMock = Mockito.mock(ScoreRepository.class);
         Mockito.when(scoreRepositoryMock.retrieveAllScores()).thenReturn(new int[] {});
-        sumService.setSomeDataService(scoreRepositoryMock);
 
         Assert.assertEquals(0, sumService.sum());
     }
 
     @Test
     public void should_sum_three_scores_using_mock_of_score_repository() {
-        SumService sumService = new SumService();
-        ScoreRepository scoreRepositoryMock = Mockito.mock(ScoreRepository.class);
         Mockito.when(scoreRepositoryMock.retrieveAllScores()).thenReturn(new int[] {1, 2, 3});
-        sumService.setSomeDataService(scoreRepositoryMock);
 
         Assert.assertEquals(6, sumService.sum());
     }
