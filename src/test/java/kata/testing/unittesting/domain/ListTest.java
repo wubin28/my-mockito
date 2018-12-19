03eded016675746279d4a66ddbd052f3809602ac
Ben WU <wubin28@gmail.com>
2018-12-19 18:21:21 +0800

    replace mock() with @Mock

--- src/test/java/kata/testing/unittesting/domain/ListTest.java
+++ src/test/java/kata/testing/unittesting/domain/ListTest.java
@@ -1,18 +1,23 @@
 package kata.testing.unittesting.domain;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListTest {
+
+    @Mock
+    List<Object> listMock;
 
     @Test
     public void should_mock_returning_once() {
-        List<Object> listMock = mock(List.class);
         when(listMock.size()).thenReturn(5);
 
         assertEquals(5, listMock.size());
@@ -20,7 +25,6 @@ public class ListTest {
 
     @Test
     public void should_mock_returning_twice() {
-        List<Object> listMock = mock(List.class);
         when(listMock.size()).thenReturn(5).thenReturn(10);
 
         assertEquals(5, listMock.size());
@@ -29,9 +33,10 @@ public class ListTest {
 
     @Test
     public void should_return_default_value_for_methods_not_mocked() {
-        List<Object> listMock = mock(List.class);
         when(listMock.get(0)).thenReturn("hello");
+
         assertEquals("hello", listMock.get(0));
         assertEquals(null, listMock.get(1));
     }
+
 }
